e3abb321e665fd26a592dd5eef91d7661401a7f5
Keero <36694122+keerobommera@users.noreply.github.com>
2025-08-27 14:35:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
+      alert('Thank you for your message! We will get back to you soon.');
+
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
